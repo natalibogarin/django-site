2157993b2757385ee93cfd626d180c7ac35fc537
natalibogarin <natali.bogarin@gmail.com>
2021-08-26 19:01:52 -0300

    Archivos configuración Heroku

--- django_site/settings/production.py
+++ django_site/settings/production.py
@@ -1,14 +1,23 @@
 # production.py
 from .base import *
+import dj_database_url
+
 DEBUG = False
-ALLOWED_HOSTS = ['myportfolio.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_site',
+        'USER': 'name',
+        'PASSWORD':'',
+        'HOST':'localhost',
+        'PORT':'',
     }
 }
 
-STATIC_URL = 'https://nombreurlproyecto.com/static/'
-MEDIA_URL = 'https://nombreurlproyecto.com/media/'
+db_from_env= dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+#STATIC_URL = 'https://nombreurlproyecto.com/static/'
+#MEDIA_URL = 'https://nombreurlproyecto.com/media/'
--- django_site/wsgi.py
+++ django_site/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhitenoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_site.settings')
 
 application = get_wsgi_application()
+application = DjangoWhitenoise(application)
