437a81b60f4a71307768b7453b071c97900be89c
natalibogarin <natali.bogarin@gmail.com>
2021-08-26 19:59:31 -0300

    SQLite3

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_site.settings.local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_site.settings.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
