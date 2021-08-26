409b2cf3c51ab8a850588861ca325a6a0057a7bb
natalibogarin <natali.bogarin@gmail.com>
2021-08-26 20:08:16 -0300

    SQLite3

--- django_site/wsgi.py
+++ django_site/wsgi.py
@@ -10,9 +10,7 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhitenoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_site.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_site.settings.production')
 
 application = get_wsgi_application()
-application = DjangoWhitenoise(application)
